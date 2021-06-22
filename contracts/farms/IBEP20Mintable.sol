2f284338feacb5fd1f3670e9e38c8ba6c8390d44
ProjectCaramelSwap <84056535+ProjectCaramelSwap@users.noreply.github.com>
2021-06-22 19:10:55 +0500

    added referral

---
+++ contracts/farms/IBEP20Mintable.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.6.0;
+
+import "./IBEP20.sol";
+
+interface IBEP20Mintable is IBEP20 {
+    function mint(address to, uint256 amount) external;
+}
